3ed1af7864678eff670e3cc6ab93a4484b457cb4
Henrique Daguerre <hdfigueiredo@outlook.com>
2025-01-22 11:33:52 -0300

    criação de componente Footer

---
+++ src/app/modules/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return (
+      <footer className="bg-gray-800 text-white py-4 mt-12">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2025 FitTech | Todos os direitos reservados.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  
+  
+  export default Footer;
