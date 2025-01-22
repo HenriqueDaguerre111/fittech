eebe12b6e7b01fbe65c0594a62d695d1b40a17ca
Henrique Daguerre <hdfigueiredo@outlook.com>
2025-01-22 11:37:02 -0300

    criação e estilização do modulo Header

---
+++ src/app/modules/Header.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import styles from '../styles/header.module.css';
+function Header() {
+    return (
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.logoContainer}>
+            <Image
+              src="/images/colorful-bird-illustration-gradient-b.png" 
+              alt="Logo"
+              width={50} 
+              height={50} 
+              className={styles.logoImage} 
+            />
+            <span className={styles.logoText}>FitTech</span>
+          </div>
+          <nav>
+            <a href="#form" className={styles.navLink}>
+              Inscreva-se
+            </a>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
